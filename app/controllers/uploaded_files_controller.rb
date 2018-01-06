2e5a292c69cdd4527e01f918cb2f3bdb432c0d2a
Bartosz Adamski <bartosz.adamski@flow2code.com>
2018-01-07 00:35:41 +0100

    List of uploaded files

--- app/controllers/uploaded_files_controller.rb
+++ app/controllers/uploaded_files_controller.rb
@@ -1,20 +1,19 @@
 class UploadedFilesController < ApplicationController
+
+  def index
+    @files = UploadedFile.all
+  end
+
   def new
     @uploaded_file = UploadedFile.new
   end
 
   def create
-    #puts params[:uploaded_file].inspect
-    puts "haha"
-    puts params[:uploaded_file][:file].inspect
-    puts params[:uploaded_file][:file].original_filename
-    #puts params[:uploaded_file][:file]
-
     @file = UploadedFile.new
     @file.name = params[:uploaded_file][:file].original_filename
     @file.file = params[:uploaded_file][:file]
     @file.save
 
-    redirect_to new_uploaded_file_path
+    redirect_to uploaded_files_path
   end
 end
